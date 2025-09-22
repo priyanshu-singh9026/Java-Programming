073c46ebd56f7049fec0e2c6edbe4559cb2f1486
priyanshu-singh9026 <psinghsingh1515@gmail.com>
2025-09-22 22:41:30 +0530

    update

---
+++ hashMAP_uses.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+import javax.print.DocFlavor.STRING;
+public class hashMAP_uses {
+    public static void main(String args[]){
+        HashMap<String, Integer> map= new HashMap<>();
+           
+
+        //INSERTING A ELEMENT
+        map.put("India",20);
+        map.put("china",30);
+        map.put("pune",40);
+        map.put("varanshi",100);
+        System.out.println(map);
+    }
+}
